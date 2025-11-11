8d64bbb49224230bc9c8522d58f9ef7b2ee759b8
Fahrul Rozi <88037312+KingOfBotch@users.noreply.github.com>
2025-11-11 15:42:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,324 @@
+<?php include "admin/config/koneksi.php" ;?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Masjid Baitul Mukminin Cimone</title>
+
+	<meta name="description" content="Design - is a Premium HTML5 Responsive Templeate by Graygrids Team.">
+
+	<meta name="keywords" content="Startup HTML template, Bootstrap Agency theme, html template, charity template, premium html template, one page template">
+
+	<meta name="author" content="GrayGrids">
+
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+
+	<!-- Bootstrap  -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- icon fonts font Awesome -->
+	<link rel="stylesheet" media="screen" href="assets/fonts/font-awesome/font-awesome.min.css">
+		
+	<!-- Custom Styles -->
+	<link href="assets/css/style.css" rel="stylesheet">
+
+	<!-- Responsive Styles -->
+	<link href="assets/css/responsive.css" rel="stylesheet">
+	
+	<!-- Normalize Styles -->
+	<link href="assets/css/normalize.css" rel="stylesheet">
+
+	<!-- Extras -->
+	<link rel="stylesheet" type="text/css" href="assets/extras/animate.css">
+	<link rel="stylesheet" type="text/css" href="assets/extras/lightbox.css">
+	<link rel="stylesheet" type="text/css" href="assets/extras/owl/owl.carousel.css">
+	<link rel="stylesheet" type="text/css" href="assets/extras/owl/owl.theme.css">
+	<link rel="stylesheet" type="text/css" href="assets/extras/owl/owl.transitions.css">
+
+	
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+</head>
+	<body>
+		<!-- Start Header Section -->
+		<header id="header">
+			<nav class="main-navigation navbar-fixed-top" role="navigation" data-spy="affix" data-offset-top="200">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+							<i class="fa fa-bars fa-lg"></i>
+						</button>
+						<!-- logo here -->
+						<a href="index.html" class="navbar-brand"></a>
+					</div>
+					
+					<!-- Start Navigation Menu -->
+					<div class="collapse navbar-collapse">
+						<ul class="nav navbar-nav navbar-right" id="main_navigation_menu">
+							<li class="active"><a href="#header">Beranda</a></li>
+							<li><a href="#services">Profil Masjid</a></li>
+							<li><a href="#blog">Laporan Keuangan</a></li>
+							<li><a href="#portfolio">Galeri</a></li>										
+							<li><a href="#contact">Donasi</a></li>
+							<li><a href="#connected">Kontak</a></li>							
+							<li><a href="admin/index.php">Login</a></li>
+						</ul>
+					</div>
+					<!-- End Navigation Menu -->
+				</div>
+			</nav>
+		</header>
+		<!-- End Header Section -->
+		
+		<!-- Start Intro Section -->
+		<section id="intro" class="section-intro">
+			<div class="overlay">
+				<div class="container">
+					<div class="row">
+						<div class="main-text">
+							<h1 class="wow fadeInUp"><span>Masjid Baitul Mukminin</span> - Cimone</h1>
+							<h2 class="wow fadeInUp" data-wow-delay=".7s">Jl. Cimone Alfa Raya No.115, RT.007/RW.007, Cimone, Kec. Karawaci, Kota Tangerang, Banten 15114</h2>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Intro Section -->
+
+		<!-- Start Services Section -->
+		<section id="services">
+			<div class="container">
+				<h1 class="section-title wow fadeIn" data-wow-delay=".2s"><span>Profil</span> Masjid</h1>
+				<div class="row">
+					<div class="col-sm-6 col-md-3 wow fadeInUp">
+						<div class="service-box">
+							<div class="content">
+								<div class="icon-wrapper">
+								<i class="fa fa-leaf"></i>
+								</div>
+								<h2>Nama Masjid</h2>
+								<p>Masjid Baitul Mukminin Cimone</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-3 wow fadeInUp" data-wow-delay=".2s" data-wow-offset="10">
+						<div class="service-box">
+							<div class="content">
+								<div class="icon-wrapper">
+								<i class="fa fa-heart"></i>
+								</div>
+								<h2>Alamat</h2>
+								<p>Jl. Cimone Alfa Raya No.115, RT.007/RW.007, Cimone, Kec. Karawaci, Kota Tangerang, Banten 15114</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-3 wow fadeInUp" data-wow-delay=".4s" data-wow-offset="10">
+						<div class="service-box">
+							<div class="content">
+								<div class="icon-wrapper">
+								<i class="fa fa-child"></i>
+								</div>
+								<h2>Sejarah singkat</h2>
+								<p>Masjid Baitul Mukminin, Berdiri sejak 17 Agustus 1985, yang berlokasi di Jl. Alfa Raya, No. 115, Cimone Permai RT : 007, RW : 007, Kelurahan Cimone, Kecamatan Karawaci, Kota Tangerang, Provinsi Banten. Dengan menggunakan area lahan seluas 11,441 m2</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-3 wow fadeInUp" data-wow-delay=".6s" data-wow-offset="10">
+						<div class="service-box">
+							<div class="content">
+								<div class="icon-wrapper">
+								<i class="fa fa-mobile-phone"></i>
+								</div>
+								<h2>Sejarah Singkat 2</h2>
+								<p>Masjid Baitul Mukminin saat ini berkapasitas 600 jama'ah. Sebelum dilakukan renovasi pada tahun 1999-2000. Masjid ini hanya mampu menampung 300 Jama'ah</p>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</section>
+		<!-- End Services Section-->
+		<!-- Start Blog Section -->
+		<section id="blog">
+			<div class="container">
+				<h1 class="section-title wow fadeIn" data-wow-delay=".2s"><span>Laporan</span> Keuangan</h1>
+				<div class="row">					
+					<div id="blog-slider" class="owl-carousel owl-theme">		
+						<?php $query=mysql_query("select * from kegiatan");
+							while($data=mysql_fetch_array($query)){
+						?>
+						<div class="item wow fadeInUp" data-wow-delay="0.4s">
+							<div class="blog-wrapper">
+							<?php
+								$gb = explode("/", $data['foto']);
+
+								$gambar = $gb[2].'/'.$gb[3].'/'.$gb[4];                  
+								?>
+								<img src="admin/<?php echo $gambar;?>" alt="" style="width:400px;height:300px">
+								<div class="content">
+									<h3><?=$data['judul_kegiatan'];?></h3>
+									<p><?=substr($data['keterangan'],0,100);?></p>
+								</div>
+							</div>
+						</div>
+						<?php }?>
+						
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Blog Section -->		
+		
+		<!-- Start Portfolio Section -->
+		<section id="portfolio">
+			<div class="container">
+			<h1 class="section-title wow fadeIn" data-wow-delay=".2s"><span>Galeri</span> <span>Kegiatan</span> Masjid</h1>
+		    	<div id="portfolio-items" class="portfolio-items wow fadeInUpQuick">
+					<?php  $query=mysql_query("SELECT * FROM `kegiatan` order by tanggal_artikel DESC limit 8");
+						while($data=mysql_fetch_array($query)){
+					?>
+						<div class="mix nwtwork" >
+							<figure>
+								<div class="img">
+								<?php
+								$gb = explode("/", $data['foto']);
+
+								$gambar = $gb[2].'/'.$gb[3].'/'.$gb[4];                  
+								?>
+								<img src="admin/<?php echo $gambar;?>" alt="" style="width:400px;height:300px">
+									<!-- <img src="assets/img/portfolio/img1.jpg" alt=""> -->
+									<div class="overlay">
+										<a data-lightbox="image1" href="assets/img/portfolio/thumb/img1.jpg" class="link-left"><i class="fa fa-plus"></i></a>
+										<a href="#" class="link-right"><i class="fa fa-link"></i></a>
+									</div>
+								</div>
+								<figcaption class="item-description">
+									<h4 class="item-title"><?=$data['judul_kegiatan'];?></h4>
+								</figcaption>
+							</figure>          
+						</div>
+					<?php  }?>
+      			</div>
+			</div>
+		</section>
+		<!-- End Portfolio Section -->
+		<!-- Start About Us Section -->
+		<section id="contact">
+			<div class="overlay">
+				<div class="container">
+				<h1 class="section-title wow fadeIn" data-wow-delay=".2s"><span>Yuk</span> Donasi!</h1>
+					<div class="row">
+						<div class="contact-form">
+							<form role="form" action="donasi/insert_donasi.php" method="post" enctype="multipart/form-data">
+								<div class="col-sm-6 col-md-6 wow fadeInLeft" data-wow-delay="0.2s">
+										<p><font color="white" /> Serta melakukan Konfirmasi transfer dengan mengupload bukti transfer melalui form dibawah ini:</p>
+										<div class="controls">
+											<input class="contact_input" type="text" name="nama_donatur" placeholder="Nama Lengkap">
+											<i class="fa fa-user"></i>
+										</div>
+									
+										<div class="controls">
+											<input class="contact_input" type="text" name="no_hp" placeholder="No. Handphone">
+											<i class="fa fa-phone"></i>
+										</div>
+										<div class="controls">
+											<input type="file" name="bukti_transfer">
+										</div>
+										
+								</div>
+								<div class="col-sm-6 col-md-6 wow fadeInRight" data-wow-delay="0.2s">
+										<p><font color="white"> Untuk melakukan donasi atau memberikan infak maupun sodaqoh melalui <b>Masjid Baitul Mukminin</b> Cimone dapat tranfer ke rekening berikut :</p>
+										<p> No.Rek BRI : 0000-0000-0000</p>
+										<p> No.Rek BNI : 0000-0000-0000</p>
+								</div>
+								<div class="col-md-6 col-md-6 wow fadeInRight" data-wow-delay="0.5s">
+											<button type="submit" id="submit" class="btn btn-common">Kirim</button>
+										</div>
+							</form>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</section>
+		<!-- End About Us Section -->	
+		
+		<!-- Start Connected Section -->
+		<section id="connected">
+			<div class="container">
+				<div class="row">
+					<h2 class="section-title wow fadeInUp" data-wow-delay=".2s">Kontak</h2>		
+					<h3 class="discription wow fadeIn" data-wow-delay=".2s">Kontak Yang Dapat DiHubungi : </h3>
+					<hr>
+					<div class="connected-wrapper text-center">
+						<div class="col-sm-6 col-md-3 wow fadeInUp" data-wow-delay="0.2s">
+							<div class="contact-item">
+							
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-3 wow fadeInUp" data-wow-delay="0.2s">
+							<div class="contact-item">
+								<a href="#"><i class="fa fa-phone"></i></a>
+								<h5>Fahrul Rozi</h5>
+								<h4>0838 79080 311</h4>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-3 wow fadeInUp" data-wow-delay="0.2s">
+							<div class="contact-item">
+								<a href="#"><i class="fa fa-user"></i></a>
+								<h5>Email</h5>
+								<h4>fahrulrozi971a@gmail.com</h4>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-3 wow fadeInUp" data-wow-delay="0.2s">
+							<div class="contact-item">
+							
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Connected Section -->
+
+		<!-- Scroll Top -->
+		<div class="scroll-top" data-spy="affix" data-offset-top="300">
+			<a href="#header"><i class="fa fa-angle-up"></i></a>
+		</div>
+		<!-- Scroll End -->
+
+		
+		<!-- Include jquery.min.js plugin -->
+		<script src="assets/js/jquery-min.js"></script>
+		<!-- Include Bootstrap plugin -->
+		<script src="assets/js/bootstrap.min.js"></script>
+		<!-- One page Nav plugin -->		
+		<script src="assets/js/jquery.nav.js"></script>
+		<!-- Include Bootstrap plugin -->
+		<script src="assets/js/owl.carousel.js"></script>
+		<!-- Include wow plugin -->
+		<script src="assets/js/wow.js"></script>
+		<!-- Include mixitup plugin -->
+		<script src="assets/js/jquery.mixitup.js"></script>
+		<!-- ScrollTop -->
+		<script src="assets/js/lightbox.min.js"></script>
+		<!-- Lightbox -->
+		<script src="assets/js/scroll-top.js"></script>
+		<!-- Smooth Scroll -->
+		<script src="assets/js/smooth-scroll.js"></script>
+		<!-- preset js -->
+		<script src="assets/js/style.changer.js"></script>
+		<!-- Modernizr js -->
+		<script src="assets/js/modernizr-2.8.0.main.js"></script>
+		<!-- Main js -->
+		<script src="assets/js/main.js"></script>
+			
+	</body>
+</html>
